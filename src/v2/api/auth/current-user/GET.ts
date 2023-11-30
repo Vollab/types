6906658632c7e4b0744188caa4227955a9404516
InSTinToS <miguelandradebarreto2@gmail.com>
2023-11-30 08:51:05 -0300

    :label: adds v2 types

---
+++ src/v2/api/auth/current-user/GET.ts
@@ -0,0 +1,14 @@
+import { Role } from '../../../../shared/role'
+
+export interface Response {
+  user: {
+    id: string
+    role: Role
+    name: string
+    phone: string
+    avatar: string
+    biography: string
+    activity_area: { id: string; name: string }
+    links: { id: string; url: string; text: string }[]
+  }
+}
