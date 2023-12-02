736eb5cda9efd1e8b1eb17d24f816d0b08810733
InSTinToS <miguelandradebarreto2@gmail.com>
2023-12-02 16:47:22 -0300

    :sparkles: adds enroll types

---
+++ src/v2/vacancy/api/demands/[demand_id]/vacancies/[vacancy_id]/enroll/POST.ts
@@ -0,0 +1,10 @@
+import { EnrollmentStatus } from '../../../../../../../shared/enrollment'
+
+export interface Params {
+  demand_id: string
+  vacancy_id: string
+}
+
+export interface Response {
+  enrollment: { status: EnrollmentStatus }
+}
