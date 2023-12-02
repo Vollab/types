9c64e546fb956064ea9369ae30b738fb3afd0c13
InSTinToS <miguelandradebarreto2@gmail.com>
2023-12-02 14:29:07 -0300

    :bug: update sing-in/

--- dist/v2/auth/api/sign-in/POST.d.ts
+++ dist/v2/auth/api/sign-in/POST.d.ts
@@ -1,3 +1,7 @@
 import { Response as CurrentUserResponse } from '../current-user/GET';
+export interface Request {
+    email: string;
+    password: string;
+}
 export interface Response extends CurrentUserResponse {
 }
--- src/v2/auth/api/sign-in/POST.ts
+++ src/v2/auth/api/sign-in/POST.ts
@@ -1,3 +1,8 @@
 import { Response as CurrentUserResponse } from '../current-user/GET'
 
+export interface Request {
+  email: string
+  password: string
+}
+
 export interface Response extends CurrentUserResponse {}
